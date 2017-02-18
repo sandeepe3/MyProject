7bfb3614c31e047976d759dc94872c13117e94d8
Sandeep <sandeep.e3@gmail.com>
2017-02-17 19:32:24 -0800

    Pearl page content
    Adding few images and perl page content

--- WebContent/js/myApp.js
+++ WebContent/js/myApp.js
@@ -21,9 +21,122 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 app.controller("emeraldCtrl", function ($scope) {
     $scope.msg = "I love London";
 });
-app.controller("pearlCtrl", function ($scope) {
+
+/*app.service('ApiCall', ['$http', function ($http){
+	var result;
+	this.postApiCall = function (obj){
+		result = $http.post('http://www.lifeofengineers.com/rest/registration/performlogin/', obj).seccess(function (data, status){
+			result = (data);
+		}).error(function(){
+			console.log("Error while invoking service");
+		});
+		return result;
+	};
+	$http({
+	    url: "http://www.lifeofengineers.com/rest/registration/performlogin",
+	    dataType: "json",
+	    method: "POST",
+	    headers: {
+	        "Content-Type": "application/json"
+	    },
+	    data: {'user_name':'anchu.venki@gmail.com', 'password': 'venkat'}
+	}).success(function(response){
+		result = response;
+	}).error(function(error){
+		console.log("Error while invoking service");
+	});
+}]);*/
+
+app.controller("pearlCtrl", function ($scope, $http) {
+	$scope.images = [{name:"Lake", price:"100", src:"images/Lake300_200.jpg"},
+	                 {name:"RiverSide", price:"100", src:"images/RiverSide300_200.jpg"},
+	                 {name:"RiverSide", price:"100", src:"images/RiverSide300_200.jpg"},
+	                 {name:"RedCar", price:"100", src:"images/RedCar300_200.jpg"},
+	                 {name:"RiverSide", price:"100", src:"images/RiverSide300_200.jpg"},
+	                 {name:"RedCar", price:"100", src:"images/RedCar300_200.jpg"},
+	                 {name:"RedCar", price:"100", src:"images/RedCar300_200.jpg"},
+	                 {name:"Lake", price:"100", src:"images/Lake300_200.jpg"},
+	                 {name:"RiverSide", price:"100", src:"images/RiverSide300_200.jpg"},
+	                 {name:"RiverSide", price:"100", src:"images/RiverSide300_200.jpg"},
+	                 {name:"RedCar", price:"100", src:"images/RedCar300_200.jpg"},
+	                 {name:"SeaSide", price:"100", src:"images/SeaSide300_200.jpg"}];
     $scope.msg = "I love Paris";
+//    var obj = {'user_name':'anchu.venki@gmail.com', 'password': 'venkat'};
+    /*var rsult = ApiCall.postApiCall(obj).success(function(data){
+    	console.log(rsult);
+    });*/
+    /*$http({
+	    url: "http://www.lifeofengineers.com/rest/registration/performlogin",
+	    dataType: "json",
+	    method: "POST",
+	    headers: {
+	        "Content-Type": "application/json"
+	    },
+	    data: {'user_name':'anchu.venki@gmail.com', 'password': 'venkat'}
+	}).success(function(response){
+		result = response;
+	}).error(function(error){
+		console.log("Error while invoking service");
+	});*/
+    
+    /*$http.get('http://rest-service.guides.spring.io/greeting').
+    then(function(response) {
+    	console.log(response.data);
+    });*/
 });
+
+/*app.factory('albumService', ['$http', '$q'], function albumService($http, $q) {
+	//interface
+	var service = {
+			albums: [],
+			getAlbums: getAlbums
+	};
+	return service;
+
+	// implementation
+	function getAlbums() {
+		var def = $q.defer();
+//		$http.get(services.path + 'student/listing', {params: {page: page, id: id}}).success(succFn).error(cmnSvc.errorHandler);
+		$http.get("http://www.lifeofengineers.com/rest/registration/performlogin", {params: {user_name: 'anchu.venki@gmail.com', password: 'venkat'}})
+			.success(function(data) {
+				service.albums = data;
+				def.resolve(data);
+			})
+			.error(function() {
+				def.reject("Failed to get albums");
+			});
+		return def.promise;
+	}
+});*/
+
+/*app.controller('pearlCtrl', ['$scope', 'albumService', function albumsController($scope, albumService) {
+	$scope.msg = "I love Paris";
+	var vm = this;
+	vm.albums = [];
+	vm.getAlbums = function() {
+		albumService.getAlbums().then(function(albums) {
+			vm.albums = albums;
+			console.log('albums returned to controller.');
+		},
+		function(data) {
+			console.log('albums retrieval failed.')
+		});
+	};
+	vm.getAlbums();
+}]);*/
+
+
+/*app.controller("pearlCtrl",function submit($scope,$http){
+	$scope.msg = "I love Paris";
+    var username = "anchu.venki@gmail.com";
+    var password=  "venkat";
+    var url = 'http://www.lifeofengineers.com/rest/registration/performlogin/';
+    console.log($http.get(url + username + "/" + password));
+});*/
+
+
+
+
 app.controller("menuController", function($scope) {
 	$scope.jewmenu = [{name:"Emerald", value:"#emerald"},
 	                  {name:"Pearl", value:"#pearl"},
